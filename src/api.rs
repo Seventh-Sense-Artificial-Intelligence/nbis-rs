1040a660d50e42af483df8825ae1657f1b726366
Varun Chatterji <varun@lemniscate.io>
2025-07-28 15:59:48 +0800

    Aligns to using top 150 minutiae for matching and storage

--- src/api.rs
+++ src/api.rs
@@ -585,6 +585,8 @@ pub fn annotate_minutiae_from_image_file(
 
 #[cfg(test)]
 mod tests {
+    use crate::ffi::DEFAULT_BOZORTH_MINUTIAE;
+
     use super::*;
     use std::fs;
 
@@ -730,7 +732,7 @@ mod tests {
         let many_minutiae_decoded = load_iso_19794_2_2005(&many_encoded).unwrap();
         assert_eq!(
             many_minutiae_decoded.inner.len(),
-            255,
+            DEFAULT_BOZORTH_MINUTIAE,
             "Decoded minutiae count should be capped at 255"
         );
     }
